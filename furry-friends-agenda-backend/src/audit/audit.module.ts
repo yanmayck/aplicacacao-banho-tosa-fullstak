496f0c27964b75f55f3025659ec18a3baad9687d
yan mayck <yanmayck108@gmail.com>
2025-10-27 19:53:36 -0300

    fix: update caniuse-lite version in package-lock.json and import PrismaModule in AuditModule

--- furry-friends-agenda-backend/src/audit/audit.module.ts
+++ furry-friends-agenda-backend/src/audit/audit.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { AuditController } from './audit.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuditController],
   providers: [AuditService],
   exports: [AuditService],
